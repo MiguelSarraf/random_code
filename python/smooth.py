64901314b903ce147ec7b9040506b6db61e5fc5f
MiguelSarraf <miguel.sarraf@usp.br>
2019-09-15 17:35:40 -0300

    adaptacao para formas abertas

--- python/smooth.py
+++ python/smooth.py
@@ -12,16 +12,22 @@ def quebra(lista):
 	res.append(lista[-1])
 	return res
 #este método recebe uma lista de float e altera cada ponto k para a media ponderada entre k, k+1 e k+2, com pesos 1,2,1
-def roda(lista):
+def roda(lista, eh_fechado):
 	'''(list)->list'''
-	lista.pop()
 	res=[]
+	if eh_fechado==True:
+		lista.pop()
+	else:
+		res.append((lista[-1]+2*lista[0]+lista[1])/4)
 	n=len(lista)
 	i=0
 	while i<n:
 		res.append((lista[i]+2*lista[(i+1)%n]+lista[(i+2)%n])/4)
 		i+=1
-	res.append(res[0])
+	if eh_fechado==True:
+		res.append(res[0])
+	else:
+		res.pop()
 	return res
 
 plt.xlabel('x')
@@ -38,14 +44,18 @@ cont=0
 while cont<len(listay):
 	listay[cont]=float(listay[cont])
 	cont+=1
+if listax[0]==listax[-1] and listay[0]==listay[-1]:
+	eh_fechado=True
+else:
+	eh_fechado=False
 print("insira o número de iterações:")
 n=int(input())
 cont=0
 while cont<n:
 	listax=quebra(listax)
 	listay=quebra(listay)
-	listax=roda(listax)
-	listay=roda(listay)
+	listax=roda(listax, eh_fechado)
+	listay=roda(listay, eh_fechado)
 	cont+=1
 plt.plot(listax, listay)
 plt.show()
