285a57f5a2791b7bcc81ce1f2a77a690cfee11c7
MiguelSarraf <miguel.sarraf@usp.br>
2019-10-04 22:52:41 -0300

    ajuste da forma para ficar mais proxima poligono original

--- python/smooth.py
+++ python/smooth.py
@@ -13,7 +13,7 @@ def quebra(lista):
 	return res
 #este método recebe uma lista de float e altera cada ponto k para a media ponderada entre k, k+1 e k+2, com pesos 1,2,1
 def roda(lista, eh_fechado):
-	'''(list)->list'''
+	'''(list, bool)->list'''
 	res=[]
 	if eh_fechado==True:
 		lista.pop()
@@ -29,7 +29,28 @@ def roda(lista, eh_fechado):
 	else:
 		res.pop()
 	return res
+#este método corrige os pontos para que os valores de entrada estejam na configuração final do desenho
+def corrige(listax, listay):
+	'''(list, list)->list, list'''
+	resx=[]
+	resy=[]
+	mediox=0
+	medioy=0
+	cont=0
+	while cont<len(listax)-1:
+		mediox+=listax[cont]
+		medioy+=listay[cont]
+		cont+=1
+	mediox/=(len(listax)-1)
+	medioy/=(len(listax)-1)
+	cont=0
+	while cont<len(listax):
+		resx.append(mediox+(5.03266031019*(len(listax)-1)**(-.851770887821))*(listax[cont]-mediox))
+		resy.append(medioy+(5.03266031019*(len(listax)-1)**(-.851770887821))*(listay[cont]-medioy))
+		cont+=1
+	return resx, resy
 
+#método main que operacionaliza os cálculos
 plt.xlabel('x')
 plt.ylabel('y')
 print("valores iniciais em x:")
@@ -44,6 +65,8 @@ cont=0
 while cont<len(listay):
 	listay[cont]=float(listay[cont])
 	cont+=1
+plt.plot(listax, listay)
+listax, listay=corrige(listax, listay)
 if listax[0]==listax[-1] and listay[0]==listay[-1]:
 	eh_fechado=True
 else:
