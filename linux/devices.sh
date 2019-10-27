af97a03042ba0ea4dcf68b3b6f9fc361e629dd97
MiguelSarraf <miguel.sarraf@usp.br>
2019-10-27 11:00:43 -0300

    nova pasta para scripts shell

---
+++ linux/devices.sh
@@ -0,0 +1,15 @@
+#this code list path directories
+#this was copied from https://unix.stackexchange.com/questions/144029/command-to-determine-ports-of-a-device-like-dev-ttyusb0
+
+#!/bin/bash
+
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname="$(udevadm info -q name -p $syspath)"
+        [[ "$devname" == "bus/"* ]] && continue
+        eval "$(udevadm info -q property --export -p $syspath)"
+        [[ -z "$ID_SERIAL" ]] && continue
+        echo "/dev/$devname - $ID_SERIAL"
+    )
+done
