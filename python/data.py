b8cf15cd2ca718866e35db35e98d9384f3af551d
MiguelSarraf <miguel.sarraf@usp.br>
2019-10-29 19:25:19 -0300

    atualizacao da rx e criacao da tx

--- python/data.py
+++ python/data.py
@@ -1,27 +1,38 @@
-import serial
 #referencia em https://makersportal.com/blog/2018/2/25/python-datalogger-reading-the-serial-output-from-arduino-to-analyze-data-using-pyserial
-def read(directory):
-	'''(str)->list'''
-	port=serial.Serial(directory, 1200, serial.SEVENBITS, serial.PARITY_EVEN, serial.STOPBITS_TWO)
+import serial
+def read(directory, baudrate=1200, databits=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_TWO):
+	'''(str, int, bit_count, bit_parity, bit_count)->list'''
+	port=serial.Serial(directory, baudrate, databits, parity, stopbits)
 	port.flushInput()
 	dados=[]
-	leitura=" "
+	leitura=None
 	cont=0
 	while cont<10:
 		try:
 			leitura=port.read(1)
-			port.write(leitura)
-			leitura=str(leitura)[2]
-			#cont=0
-		except ValueError:
-			print("erro valor")
-			#cont+=1
-		except serial.SerialException:
-			print("erro serial")
+			leitura=leitura.decode("utf-8")
+			cont=0
+		except:
+			print("error during read")
+			cont+=1
 		dados.append(leitura)
-		if dados[-1]=="p":
+		if dados[-1]==0x00000100:
 			dados.pop()
 			break
 		cont+=1
+
 	if cont==10: print("limite de erros atingido")
+	port.close()
 	return dados
+def write(directory, data, baudrate=1200, databits=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_TWO):
+	'''(str, str, int, bit_count, bit_parity, bit_count)->list'''
+	port=serial.Serial(directory, baudrate, databits, parity, stopbits)
+	n=len(data)
+	cont=0
+	while cont<n:
+		try:
+			port.write(data[cont].encode())
+		except:
+			print("error during write")
+		cont+=1
+	port.close()
