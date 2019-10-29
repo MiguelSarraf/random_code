be1b0753b73c33df79a3055ec3bd359d2a4ec504
MiguelSarraf <miguel.sarraf@usp.br>
2019-10-29 19:12:45 -0300

    alteracoes feitas no labdig

--- python/data.py
+++ python/data.py
@@ -2,18 +2,26 @@ import serial
 #referencia em https://makersportal.com/blog/2018/2/25/python-datalogger-reading-the-serial-output-from-arduino-to-analyze-data-using-pyserial
 def read(directory):
 	'''(str)->list'''
-	port=serial.Serial(directory)
+	port=serial.Serial(directory, 1200, serial.SEVENBITS, serial.PARITY_EVEN, serial.STOPBITS_TWO)
 	port.flushInput()
 	dados=[]
-	while True:
+	leitura=" "
+	cont=0
+	while cont<10:
 		try:
-			leitura=port.readLine()
-			leitura=leitura[0:len(leitura)-2].decode("utf-8")
-		except:
-			print("algum erro")
-			break
+			leitura=port.read(1)
+			port.write(leitura)
+			leitura=str(leitura)[2]
+			#cont=0
+		except ValueError:
+			print("erro valor")
+			#cont+=1
+		except serial.SerialException:
+			print("erro serial")
 		dados.append(leitura)
-		if leitura[-1]=="p":
+		if dados[-1]=="p":
 			dados.pop()
 			break
+		cont+=1
+	if cont==10: print("limite de erros atingido")
 	return dados
